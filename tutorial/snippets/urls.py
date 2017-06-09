26dcd4dea0de5aa18ca9f55c46a6e254d5592f4c
prakharchoudhary <prakhar2397@gmail.com>
2017-06-09 12:45:04 +0530

    using routers

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -75,3 +75,31 @@ urlpatterns = [
 	url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),	
 ]
 
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+# =================== USING ROUTERS =======================
+'''
+Because we're using ViewSet classes rather than View classes, we actually don't need to design the URL conf ourselves. 
+The conventions for wiring up resources into views and urls can be handled automatically, using a Router class. 
+'''
+
+# from django.conf.urls import url, include
+# from snippets import views
+# from rest_framework.routers import DefaultRouter
+
+# # Create a router and register our viewsets with it.
+# router = DefaultRouter()
+# router.register(r'snippets', views.SnippetViewSet)
+# router.register(r'users', views.UserViewSet)
+
+# # The API URLs are now determined automatically by the router.
+# # Additionally, we include the login URLs for the browsable API.
+# urlpatterns = [
+#     url(r'^', include(router.urls)),
+#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
